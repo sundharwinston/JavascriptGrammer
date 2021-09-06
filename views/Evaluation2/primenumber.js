4861398053bdc10179a74f1e4e667b81e7b6ef78
sundharwinston <sundhar102000@gmail.com>
2021-09-06 15:32:01 +0530

    correction updated

--- views/Evaluation2/primenumber.js
+++ views/Evaluation2/primenumber.js
@@ -1,14 +1,18 @@
 'use strict';
 
-let i,j,range = 200;
+let i,j,start =1,end = 200;
 
-Loop1:	for ( i = 2; i <= range; i++){
-			let flag = 0;
-			for ( j = 1; j<=i; j++){
-				if(i % j ===0) flag++;
+if(!start || !end || start > end || typeof start === 'string' || typeof end === 'string' || typeof start  === 'boolean' || typeof end === 'boolean')
+	console.log('not valid');
+else {
+	Loop1:	for ( i = start; i <= end; i++){
+				let flag = 0;
+				for ( j = 1; j<=i; j++){
+					if(i % j ===0) flag++;
+				}
+				if(flag === 2) 
+					console.log(i)
+				else
+					continue Loop1;
 			}
-			if(flag === 2) 
-				console.log(i)
-			else
-				continue Loop1;
-		}
+}
--- views/Evaluation2/zeros-count.js
+++ views/Evaluation2/zeros-count.js
@@ -1,11 +1,16 @@
-let count = 0,j,k,range = 50
-for (let i=1; i<=range; i++) {
-	j = i;
-	while ( j > 0 ) {
-		k = j % 10;
-		if (k === 0)
-			count++;
-		j = j/10;
+let count = 0,j,k,range = 50;
+
+if(!range || typeof range === 'string' || typeof range === 'boolean')
+	console.log('not valid');
+else {
+	for (let i=1; i<=range; i++) {
+		j = i;
+		while ( j > 0 ) {
+			k = j % 10;
+			if (k === 0)
+				count++;
+			j = j/10;
+		}
 	}
+	console.log(count);
 }
-console.log(count);
