b594f74dfcf372cf146f8937a980ec64e330f7ca
sundharwinston <sundhar102000@gmail.com>
2021-10-15 12:36:59 +0530

    this in arrow file edited

--- views/Evaluation3/thisInArrowFunction.js
+++ views/Evaluation3/thisInArrowFunction.js
@@ -16,7 +16,7 @@
 // ==============================================================
 
 let john = {
-	    name: 'John',
+	    name1: 'John',
 	    greet : () =>  {
 	    	this.name = "sundhar";
       		console.log(this.name);
@@ -26,8 +26,36 @@ let john = {
 	    }
 
 }
-let call = john.greet.bind(john); 
+
+let man = {
+	    name2: 'John',
+	    greet1 : () =>  {
+	    	this.name = "sundhar1";
+			this.age = 10;	
+      		console.log(this);
+	        // people.forEach(function (person) {
+	        //     console.log('Hi ' + person + '. My name is ' + that.name);
+	        // });
+	    }
+
+}
+
+let call = man.greet1.bind(john); 
 call();
+
+let human = {
+	hey : () => {
+	 console.log(this);
+   }
+ 
+ }
+ const you = {
+   a : 1,
+   b:2,
+ };
+ 
+ let check = human.hey.bind(you);
+ check();
 
 
 // ==================================================================
